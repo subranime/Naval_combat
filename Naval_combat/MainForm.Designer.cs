a0f367ce3ef2045bd18bc6931110ee64a8905267
subranime <subranime@mail.ru>
2023-12-17 17:40:44 +0400

    Улучшение реализации сервера, теперь он в бесконечном цикле ждёт сообщения от клиента.

--- Naval_combat/MainForm.Designer.cs
+++ Naval_combat/MainForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.ConnectButton = new System.Windows.Forms.Button();
+            this.SendDataButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -42,23 +43,35 @@
             // 
             // ConnectButton
             // 
-            this.ConnectButton.Location = new System.Drawing.Point(81, 127);
+            this.ConnectButton.Location = new System.Drawing.Point(73, 352);
             this.ConnectButton.Name = "ConnectButton";
-            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.Size = new System.Drawing.Size(96, 38);
             this.ConnectButton.TabIndex = 1;
-            this.ConnectButton.Text = "button1";
+            this.ConnectButton.Text = "Соеденить с сервером";
             this.ConnectButton.UseVisualStyleBackColor = true;
             this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // SendDataButton
+            // 
+            this.SendDataButton.Location = new System.Drawing.Point(238, 352);
+            this.SendDataButton.Name = "SendDataButton";
+            this.SendDataButton.Size = new System.Drawing.Size(98, 38);
+            this.SendDataButton.TabIndex = 2;
+            this.SendDataButton.Text = "Отправить данные";
+            this.SendDataButton.UseVisualStyleBackColor = true;
+            this.SendDataButton.Click += new System.EventHandler(this.SendDataButton_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SendDataButton);
             this.Controls.Add(this.ConnectButton);
             this.Controls.Add(this.richTextBox1);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.ResumeLayout(false);
 
         }
@@ -67,5 +80,6 @@
 
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button SendDataButton;
     }
 }
